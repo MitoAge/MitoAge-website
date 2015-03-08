cd2b6273186e6647fa1f5a057ca7881e33c2f7c0
Robi Tacutu <robi@unknown-2c-f0-ee-06-06-e0.home>
2015-03-08 11:26:29 +0000

    basic website functionality

--- mitoage/static_pages/urls.py
+++ mitoage/static_pages/urls.py
@@ -3,7 +3,14 @@ from django.views.generic.base import TemplateView, RedirectView
 
 urlpatterns = patterns('mitoage.static_pages.views',
 
-    url(r'^$',  RedirectView.as_view(url='about_us'), name='home'),
-    url(r'^test_page/$', TemplateView.as_view(template_name="static_pages/test_page.html"), name='test_page'),
-    url(r'^about_us/$', TemplateView.as_view(template_name="static_pages/about_us.html"), name='about_us'),
+    url(r'^$', TemplateView.as_view(template_name="static_pages/home.html"), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name="static_pages/about.html"), name='about'),
+    url(r'^user_guide/$', TemplateView.as_view(template_name="static_pages/user_guide.html"), name='user_guide'),
+    url(r'^glossary/$', TemplateView.as_view(template_name="static_pages/glossary.html"), name='glossary'),
+    url(r'^methods/$', TemplateView.as_view(template_name="static_pages/methods.html"), name='methods'),
+    url(r'^download/$', TemplateView.as_view(template_name="static_pages/download.html"), name='download'),
+    url(r'^news/$', TemplateView.as_view(template_name="static_pages/news.html"), name='news'),
+    url(r'^terms/$', TemplateView.as_view(template_name="static_pages/terms.html"), name='terms'),
+
+    #url(r'^$',  RedirectView.as_view(url='about_us'), name='home'),
 )
