139869ca3b1859138d3af6ab16de60b47c73be5c
Robi Tacutu <robi@unknown-2c-f0-ee-06-06-e0.home>
2015-07-02 19:01:48 +0100

    minor polishing and design improvements

--- mitoage/urls.py
+++ mitoage/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 
 
     # admin
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ferret/', include(admin.site.urls)),
     
     # Static and media page serving
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT} ),
