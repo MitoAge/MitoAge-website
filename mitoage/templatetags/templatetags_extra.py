14ee802bea5bde4a6bfc50a9b26b917cb3a27efa
Robi Tacutu <robi.tacutu@gmail.com>
2015-09-18 15:41:53 +0100

    p-values for pearson and other minor changes

--- mitoage/templatetags/templatetags_extra.py
+++ mitoage/templatetags/templatetags_extra.py
@@ -16,6 +16,18 @@ def field_in_dictionary(row, key):
 def per_1kb(value, size):
     return int(round(value*1000.0/size, 0)) if value and size else None
 
+
+@register.filter
+def pearson_p_value(r, n):
+    df = n-2
+    if abs(r) == 1.0:
+        p = 0.0
+    else:
+        from scipy.stats.stats import betai
+        t_squared = r*r * (df / ((1.0 - r) * (1.0 + r)))
+        p = betai(0.5*df, 0.5, df / (df + t_squared))
+    return p
+
 @register.simple_tag
 def percent(value, size, digits):
     value = Decimal(value)
